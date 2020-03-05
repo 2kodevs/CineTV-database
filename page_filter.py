6ef8ca97d8732cd2758de96bcfbf28abd0c79847
Lazaro Raul <riglesiasvera@gmail.com>
2020-03-04 19:52:45 -0500

    [parser] refs #3 - Detecting files important pages

---
+++ page_filter.py
@@ -0,0 +1,25 @@
+import os
+from pdftotext import PDF
+
+def main(args):
+    files = [x for x in os.listdir(args.path) if x[-4:] == ".pdf"]
+
+    for file in files:
+        with open(f"{args.path}/{file}", 'rb') as fd:
+            pdf = PDF(fd)
+        idx, name = 0, file.split('.')[0]
+        for page in pdf:
+            line = page.split("\n")[0].split()
+            if len(line) == 10 and "CARTELERA" in line:
+                with open(f"{args.folder}/{name}-page{idx}.txt", 'w') as fd:
+                    fd.write(page)
+    
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='CineTV page filter')
+    parser.add_argument('-p', '--path', type=str, default='Cartelera JR 2019/', help='path of the pdf files')
+    parser.add_argument('-f', '--folder', type=str, default='Cartelera JR 2019/data', help='destination folder')
+    
+    args = parser.parse_args()
+    main(args)
